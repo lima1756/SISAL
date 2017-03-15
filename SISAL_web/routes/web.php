512948cc4b87ae23ff71cf0c4afaaa94c1e2ccf3
JosFran21 <francisco.mtzc@hotmail.com>
2017-03-15 00:58:16 -0600

    Rutas
    Las rutas correspondientes a los perfiles principales

--- SISAL_web/routes/web.php
+++ SISAL_web/routes/web.php
@@ -21,9 +21,16 @@ Route::get('/home', function () {
 });
 
 Route::get('/doctor', function () {
-    return view('/../doctor/index');
+    return view('doctor/index');
 });
 
 Route::get('/admin', function () {
-    return view('/../admin/index');
+    return view('admin/index');
+});
+Route::get('/patient', function () {
+    return view('patient/index');
+});
+
+Route::get('/receptionist', function () {
+    return view('receptionist/index');
 });
