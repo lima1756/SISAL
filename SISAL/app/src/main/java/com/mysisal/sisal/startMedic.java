b207d2e11fb4cb8cc4914ee858b5e47c05d01a41
lima1756 <luisivanmorett@gmail.com>
2017-06-01 15:58:24 -0500

    Metodo sin uso

--- SISAL/app/src/main/java/com/mysisal/sisal/startMedic.java
+++ SISAL/app/src/main/java/com/mysisal/sisal/startMedic.java
@@ -242,20 +242,6 @@ public class startMedic extends AppCompatActivity
                 });
         return true;
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.logOut) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
